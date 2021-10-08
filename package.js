dd88c0d3830ac09e8419ba28086fed4d02561fb7
Jan Dvorak <storyteller@freedombase.net>
2021-10-08 13:44:35 +0200

    Deprecate the package

--- package.js
+++ package.js
@@ -1,19 +1,20 @@
 Package.describe({
   name: 'storyteller:accounts-react-materialize',
-  version: '0.1.3',
+  version: '0.1.4',
   // Brief, one-line summary of the package.
   summary: 'Front end for accounts login, registration and settings with react and Materialize.',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/StorytellerCZ/meteor-accounts-react-materialize.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  deprecated: true
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  api.versionsFrom(['1.3', '2.3']);
   //mdg packages used
-  api.use(['meteor', 'ecmascript', 'react@0.14.3', 'check', 'accounts-password']);
+  api.use(['meteor', 'ecmascript', 'check', 'accounts-password']);
 
   //other packages in use
   api.use(['kadira:flow-router@2.10.0', 'storyteller:accounts-server@0.1.1']);
